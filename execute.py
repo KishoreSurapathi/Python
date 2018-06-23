ee57c700e22d46045bb38e4e76ca24eac8afa395
linux root user <kishorekumar183@gmail.com>
2018-06-22 21:29:25 -0500

    added files

---
+++ execute.py
@@ -0,0 +1,29 @@
+from statistics import mean
+
+listex = [1,2,3,4,5,6,7]
+
+print (mean(listex))
+
+
+
+class calc:
+    def add(x,Y):
+      answer = x + Y
+      print(answer)	
+    def sub(x,Y):
+      answer = x - Y
+      print(answer)
+    def mult(x,Y):
+      answer = x * Y
+      print(answer)
+    def div(x,Y):
+      answer = x / Y
+      print(answer)
+
+
+
+
+
+
+myans = calc.add(5,3)
+print(myans)	
