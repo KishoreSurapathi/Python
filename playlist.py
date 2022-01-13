1256e4afb80206ff9b57d630ec2286b4eb65a0f0
kishoreCodes <KishoreSurapathi@users.noreply.github.com>
2022-01-13 16:06:45 -0500

    Update playlist.py

--- playlist.py
+++ playlist.py
@@ -8,14 +8,11 @@ class Playlist:
     def output_song(self):
         song = random.choice(self.songs)
         print("From " + str(self.songs) + "        Played " + str(song))
+        self.songs.remove(song)
+        self.karray.append(song)
         if len(self.karray) >= self.k:
-            self.songs.remove(song)
-            self.karray.append(song)
             self.songs.append(self.karray.pop(0))
-        else:
-            self.songs.remove(song)
-            self.karray.append(song)
 
 play = Playlist(2)
-for i in range(6):
+for i in range(7):
     play.output_song()
